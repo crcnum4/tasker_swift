f02d4c82f64b865b898cf112fdbc4ca847b65dae
Cliff Choiniere <cliffc@cliffs-mbp.fios-router.home>
2017-01-21 11:37:30 -0500

    Initial Commit

---
+++ tasker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tasker
+//
+//  Created by Cliff Choiniere on 1/21/17.
+//  Copyright © 2017 Cliff Choiniere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
